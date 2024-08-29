163480921ca7e3d8aaefdd85bdc9cb11eb0d99e9
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2024-08-29 22:27:14 +0530

    feat: add break/finished msg (#4)
    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,17 @@ use chrono::DateTime;
 use std::fs::{self};
 use std::path::PathBuf;
 
-const BREAK_INDICATOR: &str = "\\o/";
+pub const DEFAULT_PENDING_BLOCK: &str = "▫";
+pub const DEFAULT_COMPLETE_BLOCK: &str = "▪";
+pub const DEFAULT_LEFT_PAD: &str = " ";
+pub const DEFAULT_RIGHT_PAD: &str = " ";
+pub const DEFAULT_DELIMITER: &str = "";
+pub const DEFAULT_NUM_BLOCKS: u8 = 10;
+pub const DEFAULT_FINISHED_MSG: &str = "done";
+pub const DEFAULT_BREAK_MSG: &str = "\\o/";
+
+pub const BREAK_STRING: &str = "break";
+pub const STOP_STRING: &str = "stop";
 
 pub struct DisplayConfig {
     pub pending_block: String,
@@ -13,6 +23,23 @@ pub struct DisplayConfig {
     pub right_pad: String,
     pub delimiter: String,
     pub num_blocks: u8,
+    pub finished_msg: String,
+    pub break_msg: String,
+}
+
+impl Default for DisplayConfig {
+    fn default() -> Self {
+        DisplayConfig {
+            pending_block: DEFAULT_PENDING_BLOCK.to_string(),
+            complete_block: DEFAULT_COMPLETE_BLOCK.to_string(),
+            left_pad: DEFAULT_LEFT_PAD.to_string(),
+            right_pad: DEFAULT_RIGHT_PAD.to_string(),
+            delimiter: DEFAULT_DELIMITER.to_string(),
+            num_blocks: DEFAULT_NUM_BLOCKS,
+            finished_msg: DEFAULT_FINISHED_MSG.to_string(),
+            break_msg: DEFAULT_BREAK_MSG.to_string(),
+        }
+    }
 }
 
 pub fn start_tracking(file_path: &PathBuf, time: DateTime<Utc>) -> Result<()> {
@@ -20,32 +47,33 @@ pub fn start_tracking(file_path: &PathBuf, time: DateTime<Utc>) -> Result<()> {
         .with_context(|| format!("could not write to file `{:?}`", file_path))
 }
 
-pub fn take_break(file_path: &PathBuf, break_string: &str) -> Result<()> {
-    fs::write(file_path, break_string)
+pub fn take_break(file_path: &PathBuf) -> Result<()> {
+    fs::write(file_path, BREAK_STRING)
         .with_context(|| format!("couldn't write to file: `{:?}`", file_path))
 }
 
-pub fn stop_tracking(file_path: &PathBuf, stop_string: &str) -> Result<()> {
-    fs::write(file_path, stop_string)
+pub fn stop_tracking(file_path: &PathBuf) -> Result<()> {
+    fs::write(file_path, STOP_STRING)
         .with_context(|| format!("couldn't write to file: `{:?}`", file_path))
 }
 
 pub fn show_progress(
     file_path: &PathBuf,
     now: DateTime<Utc>,
-    break_string: &str,
-    stop_string: &str,
-    config: DisplayConfig,
+    config: &DisplayConfig,
 ) -> Result<()> {
     let status = fs::read_to_string(file_path)
         .with_context(|| format!("couldn't not read from file {:?}", file_path))?;
 
-    if status == stop_string {
+    if status == STOP_STRING {
         return Ok(());
     }
 
-    if status == break_string {
-        print!("{}{}{}", config.left_pad, break_string, config.right_pad,);
+    if status == BREAK_STRING {
+        print!(
+            "{}{}{}",
+            config.left_pad, config.break_msg, config.right_pad,
+        );
         return Ok(());
     }
 
@@ -55,11 +83,20 @@ pub fn show_progress(
         .with_context(|| "couldn't not parse time from tomo's data file {:?}")?;
 
     let diff_seconds = now.signed_duration_since(ts.to_utc()).num_seconds();
+    let output = get_progress_bar(diff_seconds, config);
+    println!("{}", output);
+
+    Ok(())
+}
+
+pub fn get_progress_bar(diff_seconds: i64, config: &DisplayConfig) -> String {
     let chunks = diff_seconds / (25 * 60 / (config.num_blocks as i64));
 
     if chunks >= config.num_blocks as i64 {
-        print!("{}{}{}", config.left_pad, BREAK_INDICATOR, config.right_pad,);
-        return Ok(());
+        return format!(
+            "{}{}{}",
+            config.left_pad, config.finished_msg, config.right_pad,
+        );
     }
 
     let mut bar = String::new();
@@ -77,9 +114,139 @@ pub fn show_progress(
             bar.push_str(&config.delimiter);
         }
     }
+
     bar.push_str(&config.pending_block);
 
-    print!("{}{}{}", config.left_pad, bar, config.right_pad,);
+    format!("{}{}{}", config.left_pad, bar, config.right_pad,)
+}
 
-    Ok(())
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_progress_works_with_defaults() {
+        // GIVEN
+        let config = DisplayConfig::default();
+
+        // WHEN
+        let got = get_progress_bar(10 * 60, &config);
+
+        // THEN
+        let expected = String::from(" ▪▪▪▪▫▫▫▫▫▫ ");
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn get_progress_respects_custom_padding() {
+        // GIVEN
+        let default_config = DisplayConfig::default();
+        let config = DisplayConfig {
+            left_pad: String::from("["),
+            right_pad: String::from("]"),
+            ..default_config
+        };
+
+        // WHEN
+        let got = get_progress_bar(0, &config);
+
+        // THEN
+        let expected = String::from("[▫▫▫▫▫▫▫▫▫▫]");
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn get_progress_respects_custom_blocks() {
+        // GIVEN
+        let default_config = DisplayConfig::default();
+        let config = DisplayConfig {
+            pending_block: String::from("-"),
+            complete_block: String::from("+"),
+            ..default_config
+        };
+
+        // WHEN
+        let got = get_progress_bar(18 * 60, &config);
+
+        // THEN
+        let expected = String::from(" +++++++--- ");
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn get_progress_respects_num_blocks() {
+        // GIVEN
+        let default_config = DisplayConfig::default();
+        let config = DisplayConfig {
+            num_blocks: 5,
+            ..default_config
+        };
+
+        // WHEN
+        let got = get_progress_bar(10 * 60, &config);
+
+        // THEN
+        let expected = String::from(" ▪▪▫▫▫ ");
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn get_progress_respects_custom_delimiter() {
+        // GIVEN
+        let default_config = DisplayConfig::default();
+        let config = DisplayConfig {
+            delimiter: String::from(" "),
+            ..default_config
+        };
+
+        // WHEN
+        let got = get_progress_bar(10 * 60, &config);
+
+        // THEN
+        let expected = String::from(" ▪ ▪ ▪ ▪ ▫ ▫ ▫ ▫ ▫ ▫ ");
+
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn get_progress_shows_empty_progress_bar() {
+        // GIVEN
+        let config = DisplayConfig::default();
+
+        // WHEN
+        let got_at_start = get_progress_bar(0, &config);
+        let got_at_min_one = get_progress_bar(1 * 60, &config);
+
+        // THEN
+        let expected = String::from(" ▫▫▫▫▫▫▫▫▫▫ ");
+
+        assert_eq!(got_at_start, expected);
+        assert_eq!(got_at_min_one, expected);
+    }
+
+    #[test]
+    fn get_progress_shows_finished_msg_when_timer_is_finished() {
+        // GIVEN
+        let default_config = DisplayConfig::default();
+        let config = DisplayConfig {
+            left_pad: String::from("[["),
+            right_pad: String::from("]]"),
+            finished_msg: String::from("fertig"),
+            ..default_config
+        };
+
+        // WHEN
+        let got_at_min_25 = get_progress_bar(25 * 60, &config);
+        let got_at_min_26 = get_progress_bar(26 * 60, &config);
+
+        // THEN
+        let expected = String::from("[[fertig]]");
+
+        assert_eq!(got_at_min_25, expected);
+        assert_eq!(got_at_min_26, expected);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,9 @@ use clap::{Parser, Subcommand};
 use dirs::data_dir;
 use std::fs;
 use std::path::PathBuf;
-use tomo::{show_progress, start_tracking, stop_tracking, take_break, DisplayConfig};
 
 const DATA_DIR: &str = "tomo";
 const DATA_FILE: &str = ".tomo";
-const STOP_STRING: &str = "stop";
-const BREAK_STRING: &str = "break";
 const ELAPSED_MINS_UPPER_LIMIT: u8 = 20;
 
 /// tomo is a no-frills pomodoro progress indicator intended for tmux and similar terminal multiplexers
@@ -21,28 +18,36 @@ struct Args {
     action: Option<Action>,
     /// String to represent a "pending" block in the progress bar
     #[arg(short = 'p', long = "pending-block", value_name = "STRING")]
-    #[clap(default_value = "▫")]
+    #[clap(default_value = tomo::DEFAULT_PENDING_BLOCK)]
     pending_block: String,
     /// String to represent a "complete" block in the progress bar
     #[arg(short = 'c', long = "complete-block", value_name = "STRING")]
-    #[clap(default_value = "▪")]
+    #[clap(default_value = tomo::DEFAULT_COMPLETE_BLOCK)]
     complete_block: String,
     /// String to pad the output with on the LHS
     #[arg(short = 'l', long = "left-pad", value_name = "STRING")]
-    #[clap(default_value = " ")]
+    #[clap(default_value = tomo::DEFAULT_LEFT_PAD)]
     left_pad: String,
     /// String to pad the output with on the RHS
     #[arg(short = 'r', long = "right-pad", value_name = "STRING")]
-    #[clap(default_value = " ")]
+    #[clap(default_value = tomo::DEFAULT_RIGHT_PAD)]
     right_pad: String,
     /// Delimiter between progress bar chunks
     #[arg(short = 'd', long = "delimiter", value_name = "STRING")]
-    #[clap(default_value = "")]
+    #[clap(default_value = tomo::DEFAULT_DELIMITER)]
     delimiter: String,
     /// Number of blocks to show in progress bar
     #[arg(short = 'n', long = "num-blocks", value_name = "NUM")]
-    #[clap(default_value = "10")]
+    #[clap(default_value_t = tomo::DEFAULT_NUM_BLOCKS)]
     num_blocks: u8,
+    /// Message to show when timer is finished
+    #[arg(long = "finished-msg", value_name = "STRING")]
+    #[clap(default_value = tomo::DEFAULT_FINISHED_MSG)]
+    finished_msg: String,
+    /// Message to show when on a break
+    #[arg(long = "break-msg", value_name = "STRING")]
+    #[clap(default_value = tomo::DEFAULT_BREAK_MSG)]
+    break_msg: String,
 }
 
 #[derive(Subcommand, Debug, Clone)]
@@ -84,16 +89,18 @@ fn main() {
 
     let result = match args.action {
         None => {
-            let config = DisplayConfig {
+            let config = tomo::DisplayConfig {
                 pending_block: args.pending_block,
                 complete_block: args.complete_block,
                 left_pad: args.left_pad,
                 right_pad: args.right_pad,
                 delimiter: args.delimiter,
                 num_blocks: args.num_blocks,
+                finished_msg: args.finished_msg,
+                break_msg: args.break_msg,
             };
 
-            show_progress(&data_file_path, now, BREAK_STRING, STOP_STRING, config)
+            tomo::show_progress(&data_file_path, now, &config)
         }
         Some(Action::Start { elapsed_mins }) => {
             if elapsed_mins > ELAPSED_MINS_UPPER_LIMIT {
@@ -103,13 +110,13 @@ fn main() {
                 );
                 process::exit(1);
             }
-            start_tracking(
+            tomo::start_tracking(
                 &data_file_path,
                 now - Duration::minutes(elapsed_mins as i64),
             )
         }
-        Some(Action::Stop) => stop_tracking(&data_file_path, STOP_STRING),
-        Some(Action::Break) => take_break(&data_file_path, BREAK_STRING),
+        Some(Action::Stop) => tomo::stop_tracking(&data_file_path),
+        Some(Action::Break) => tomo::take_break(&data_file_path),
     };
 
     if let Err(e) = result {
